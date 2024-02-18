af0566b8cfcc7b01001267a45bc52c1fa8b6dae6
hoangnvthkt <hoangnvthkt@gmail.com>
2024-02-18 15:32:23 +0700

    video 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,15 @@
+// class component
+// function component
+import React from "react";
+
+class Mycomponent extends React.Component {
+  //JSX
+  render() {
+    return (
+      <>
+        <div>My first component</div>
+      </>
+    );
+  }
+}
+export default Mycomponent;
