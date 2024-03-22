86aaa38fe3a3dbe005a58901cfbf6307d19b4ae7
hoangnvthkt <hoangnvthkt@gmail.com>
2024-03-22 10:50:10 +0700

    video 83

--- src/components/Users/DetailQuiz.js
+++ src/components/Users/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiServices";
+import _ from "lodash";
 const DetailQuiz = (props) => {
   const params = useParams();
   const quizId = params.id;
@@ -10,6 +11,26 @@ const DetailQuiz = (props) => {
 
   const fetchQuestions = async () => {
     let res = await getDataQuiz(quizId);
+    if (res && res.EC === 0) {
+      let raw = res.DT;
+      let data = _.chain(raw)
+        .groupBy("id")
+        .map((value, key) => {
+          let answers = [];
+          let questionDescription,
+            image = null;
+          value.forEach((item, index) => {
+            if (index === 0) {
+              questionDescription = item.description;
+              image = item.image;
+            }
+            answers.push(item.answers);
+          });
+          return { questionId: key, answers, questionDescription, image };
+        })
+        .value();
+      console.log(data);
+    }
   };
   return <div className="detail-quiz-container">Detail quiz</div>;
 };
