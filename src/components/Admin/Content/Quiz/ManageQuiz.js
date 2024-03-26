e6ad94c804fb465a4d7fe54a2666a47b91e4fdbb
hoangnvthkt <hoangnvthkt@gmail.com>
2024-03-26 16:39:09 +0700

    video 90

--- src/components/Admin/Content/Quiz/ManageQuiz.js
+++ src/components/Admin/Content/Quiz/ManageQuiz.js
@@ -1,6 +1,8 @@
 import "./ManageQuiz.scss";
 import Select from "react-select";
 import { useState } from "react";
+import { postCreateNewQuiz } from "../../../../services/apiServices";
+import { toast } from "react-toastify";
 const options = [
   { value: "EASY", label: "EASY" },
   { value: "MEDIUM", label: "MEDIUM" },
@@ -12,7 +14,27 @@ const ManageQuiz = (props) => {
   const [type, setType] = useState("");
   const [image, setImage] = useState(null);
 
-  const handleChangFile = (event) => {};
+  const handleChangFile = (event) => {
+    if (event.target && event.target.files && event.target.files[0]) {
+      setImage(event.target.files[0]);
+    }
+  };
+  const handleSubmitQuiz = async () => {
+    //validate
+    if (!name || !description) {
+      toast.error("Name/Description is required");
+      return;
+    }
+    let res = await postCreateNewQuiz(description, name, type?.value, image);
+    if (res && res.EC === 0) {
+      toast.success(res.EM);
+      setName("");
+      setDescription("");
+      setImage(null);
+    } else {
+      toast.error(res.EM);
+    }
+  };
   return (
     <div className="quiz-container">
       <div className="title">Manage Quizzes</div>
@@ -43,7 +65,12 @@ const ManageQuiz = (props) => {
           <label for="floatingPassword">Description</label>
         </div>
         <div className="my-3">
-          <Select value={type} options={options} placeholder={"Quiz type"} />
+          <Select
+            defaultValue={type}
+            onChange={setType}
+            options={options}
+            placeholder={"Quiz type"}
+          />
         </div>
         <div className="more-actions form-group">
           <label className="mb-1">Upload Image</label>
@@ -52,6 +79,14 @@ const ManageQuiz = (props) => {
             className="form-control"
             onChange={(event) => handleChangFile(event)}
           />
+        </div>
+        <div className="mt-3">
+          <button
+            onClick={() => handleSubmitQuiz()}
+            className="btn btn-warning"
+          >
+            Save
+          </button>
         </div>
       </fieldset>
       <div className="list-detail">Table</div>
--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -59,6 +59,14 @@ const postSubmitQuiz = (data) => {
   return axios.post(`/api/v1/quiz-submit`, { ...data });
 };
 
+const postCreateNewQuiz = (description, name, difficulty, image) => {
+  const data = new FormData();
+  data.append("description", description);
+  data.append("name", name);
+  data.append("difficulty", difficulty);
+  data.append("image", image);
+  return axios.post(`api/v1/quiz`, data);
+};
 export {
   postCreateNewUser,
   getAllUsers,
@@ -70,4 +78,5 @@ export {
   getQuizByUser,
   getDataQuiz,
   postSubmitQuiz,
+  postCreateNewQuiz,
 };
